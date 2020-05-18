9c9f3be14e44016b5ce99cdcbc6769ee2d143817
IvanPanov53 <panyruk1992@gmail.com>
2020-05-18 14:44:16 +0300

    feat: add js function for modal dialog

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,15 @@
-console.log('Скрипт загрузился');
+document.addEventListener("DOMContentLoaded", function () {
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]')
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  }
+    
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+});
